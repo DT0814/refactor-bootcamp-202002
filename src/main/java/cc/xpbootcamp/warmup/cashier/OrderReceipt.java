cc39c448f8346bad941279d8b2ead7e6ed4be1f3
DT0814 <729742011@qq.com>
2020-02-15 17:18:25 +0800

    magical value

--- src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
@@ -3,6 +3,10 @@ package cc.xpbootcamp.warmup.cashier;
 public class OrderReceipt {
     private Order order;
 
+    private static String TITLE = "======Printing Orders======\n";
+    private static String SALES_TAX = "Sales Tax";
+    private static String TOTAL_AMOUNT = "Total Amount";
+
     public OrderReceipt(Order order) {
         this.order = order;
     }
@@ -10,15 +14,15 @@ public class OrderReceipt {
     public String printReceipt() {
         StringBuilder output = new StringBuilder();
 
-        output.append("======Printing Orders======\n");
+        output.append(TITLE);
 
         output.append(order.getCustomerName());
         output.append(order.getCustomerAddress());
 
         printItems(output, order);
 
-        output.append("Sales Tax").append('\t').append(order.getTotalSalesTransaction());
-        output.append("Total Amount").append('\t').append(order.getTotal());
+        output.append(SALES_TAX).append('\t').append(order.getTotalSalesTransaction());
+        output.append(TOTAL_AMOUNT).append('\t').append(order.getTotal());
 
         return output.toString();
     }
