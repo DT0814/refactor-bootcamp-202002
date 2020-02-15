ab4df1a855567b2d631314416968a5f7e53d4553
DT0814 <729742011@qq.com>
2020-02-15 16:47:31 +0800

    refactor

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -1,9 +1,15 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
 public class Fibonacci {
+    private static long arr[] = new long[50];
+
     public static long calculate(int i) {
         if (i > 2) {
-            return calculate(i - 1) + calculate(i - 2);
+            long first = arr[i-1] == 0 ? calculate(i-1) : arr[i-1];
+            arr[i-1] = first;
+            long second = arr[i-2] == 0 ? calculate(i-2) : arr[i-2];
+            arr[i-2] = second;
+            return first + second;
         }
         return 1;
     }
