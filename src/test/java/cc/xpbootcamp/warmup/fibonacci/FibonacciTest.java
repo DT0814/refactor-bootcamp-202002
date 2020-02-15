b0e70eae92947d4d2c967af7445a65573fbda893
DT0814 <729742011@qq.com>
2020-02-15 16:32:55 +0800

    should_be_return_1_when_calculate_given_position_is_1

---
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -0,0 +1,16 @@
+package cc.xpbootcamp.warmup.fibonacci;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FibonacciTest {
+    @Test
+    void should_be_return_1_when_calculate_given_position_is_1() {
+        //Arrange
+
+        //Act
+        int result = Fibonacci.calculate(1);
+        //Assert
+        Assertions.assertEquals(1, result);
+    }
+}
