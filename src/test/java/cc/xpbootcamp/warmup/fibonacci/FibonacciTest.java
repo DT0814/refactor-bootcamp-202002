3b172aceb1bea119ed8775ac48ef8e3ca170a17a
DT0814 <729742011@qq.com>
2020-02-15 16:39:31 +0800

    should_be_return_2_when_calculate_given_position_is_3

--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -15,12 +15,22 @@ public class FibonacciTest {
     }
 
     @Test
-    void should_be_return_2_when_calculate_given_position_is_1() {
+    void should_be_return_1_when_calculate_given_position_is_2() {
         //Arrange
 
         //Act
         int result = Fibonacci.calculate(2);
         //Assert
         Assertions.assertEquals(1, result);
+    }
+
+    @Test
+    void should_be_return_2_when_calculate_given_position_is_3() {
+        //Arrange
+
+        //Act
+        int result = Fibonacci.calculate(3);
+        //Assert
+        Assertions.assertEquals(2, result);
     }
 }
